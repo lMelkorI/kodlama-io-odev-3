94d9f25cf5ca6c23c3d7adfc9bd5dbb7ebe4dc03
lMelkorI <108741628+lMelkorI@users.noreply.github.com>
2023-04-05 12:00:52 +0300

    Add files via upload

---
+++ kodlamaioödev3.py
@@ -0,0 +1,23 @@
+#hyper text markup language
+#hiper metin işaretleme dili
+#html
+#web sayfası oluşturmak için kullanılır ve bir programlama dili olarak geçmez
+#çünkü html kodlarıyla kendi başına çalışan bir program yazılamaz
+#ama bu dili yorumlayabilen programlar aracılığıyla programlar yazılabilir
+##############################3
+#html locators
+#Locators(Konumlantırıcı) Selenium IDE'ye hangi web tabanlı objeler üzerinde çalışması gerektiğini söyleyen bir komuttur
+##############################
+#Selenium da action class ı selenium un klavye ve fare girdilerini işleyebilmemize olanak sağlayan bir sınıftır
+#Fare için 
+#doubleClick()
+#clickAndHold()
+#dragAndDrop()
+#moveToElement()
+#contextClick()
+#Klavye için
+#sendKeys()
+#keyUp()
+#keyDown()
+#fonksiyonları action class fonksiyonlarıdır
+#
